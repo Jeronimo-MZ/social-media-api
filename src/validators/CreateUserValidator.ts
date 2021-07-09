2d9b3f74aa366d2933e0e7aef8db1345a5d67527
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-09 13:25:44 +0200

    update createUserValidator

--- src/validators/CreateUserValidator.ts
+++ src/validators/CreateUserValidator.ts
@@ -4,9 +4,9 @@ import { AppError } from "../errors/AppError";
 class CreateUserValidator {
     async validate(object: Object): Promise<void> {
         const schema = Yup.object({
-            nickname: Yup.string().min(3).required(),
-            email: Yup.string().email().required(),
-            password: Yup.string().min(6).required(),
+            nickname: Yup.string().min(3).max(20).required(),
+            email: Yup.string().email().max(50).required(),
+            password: Yup.string().min(6).max(30).required(),
         });
 
         try {
