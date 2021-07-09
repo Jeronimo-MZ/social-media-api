7ed0a63cf97e3e095b3bbb56815f2f7eeae55f92
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-09 15:11:26 +0200

    update users Repository

--- src/repositories/IUsersRepository.ts
+++ src/repositories/IUsersRepository.ts
@@ -1,8 +1,14 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { IUpdateUserDTO } from "../dtos/IUpdateUserDTO";
 import { IUser } from "../models/User";
 
 export interface IUsersRepository {
     create(data: ICreateUserDTO): Promise<IUser>;
     findByEmail(email: string): Promise<IUser | undefined>;
     findByNickname(nickname: string): Promise<IUser | undefined>;
+    findById(id: string): Promise<IUser | undefined>;
+    update(
+        user_id: string,
+        newData: IUpdateUserDTO
+    ): Promise<IUser | undefined>;
 }
--- src/repositories/implementations/UsersRepository.ts
+++ src/repositories/implementations/UsersRepository.ts
@@ -1,4 +1,5 @@
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUpdateUserDTO } from "../../dtos/IUpdateUserDTO";
 import User, { IUser } from "../../models/User";
 import { IUsersRepository } from "../IUsersRepository";
 
@@ -21,11 +22,27 @@ export default class UsersRepository implements IUsersRepository {
 
     public async findByEmail(email: string): Promise<IUser | undefined> {
         const user = await User.findOne({ email });
-        return user || undefined;
+        return user?.toObject() || undefined;
     }
 
     public async findByNickname(nickname: string): Promise<IUser | undefined> {
         const user = await User.findOne({ nickname });
-        return user || undefined;
+        return user?.toObject() || undefined;
+    }
+
+    public async findById(id: string): Promise<IUser | undefined> {
+        const user = await User.findById(id);
+        return user?.toObject() || undefined;
+    }
+
+    public async update(
+        user_id: string,
+        newData: IUpdateUserDTO
+    ): Promise<IUser | undefined> {
+        await User.findByIdAndUpdate(user_id, newData);
+
+        const updatedUser = await User.findById(user_id);
+
+        return updatedUser?.toObject() || undefined;
     }
 }
