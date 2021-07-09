9024e042e2e66ba2f32865eaa977efd33db3c54d
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-09 11:57:33 +0200

    add BCryptHashProvider

---
+++ src/validators/providers/HashProvider/implementations/BCryptHashProvider.ts
@@ -0,0 +1,13 @@
+import { IHashProvider } from "../IHashProvider";
+import bcrypt from "bcryptjs";
+
+class BCryptHashProvider implements IHashProvider {
+    async generateHash(payload: string): Promise<string> {
+        return bcrypt.hash(payload, 10);
+    }
+    async compareHash(payload: string, hashed: string): Promise<boolean> {
+        return bcrypt.compare(payload, hashed);
+    }
+}
+
+export { BCryptHashProvider };
