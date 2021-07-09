a13a49a407c74fef832d690596fe324a4d6db176
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-09 11:59:48 +0200

    add bcrypyHashProvider to createUserService params

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -2,10 +2,14 @@ import { Request, Response } from "express";
 import UsersRepository from "../repositories/implementations/UsersRepository";
 import CreateUserService from "../services/CreateUserService";
 import { CreateUserValidator } from "../validators/CreateUserValidator";
+import { BCryptHashProvider } from "../validators/providers/HashProvider/implementations/BCryptHashProvider";
 
 class UsersController {
     async create(request: Request, response: Response): Promise<Response> {
-        const createUser = new CreateUserService(new UsersRepository());
+        const createUser = new CreateUserService(
+            new UsersRepository(),
+            new BCryptHashProvider()
+        );
         const validator = new CreateUserValidator();
 
         await validator.validate(request.body);
