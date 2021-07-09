e2486ee6c320480ba576ae160cd970074fdd2144
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-09 12:58:12 +0200

    add sessions routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { userRouter } from "./UserRouter";
+import sessionsRouter from "./sessions.routes";
+import { userRouter } from "./users.routes";
 
 const router = Router();
 
 router.use("/users", userRouter);
+router.use("/sessions", sessionsRouter);
 
 export { router };
