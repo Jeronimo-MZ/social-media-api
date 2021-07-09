441dee0f47da0efea2ae310c2076da2cd3227402
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-09 12:57:59 +0200

    register tokenProvider

--- src/container/providers/index.ts
+++ src/container/providers/index.ts
@@ -1,5 +1,8 @@
 import { container } from "tsyringe";
 import { IHashProvider } from "./HashProvider/models/IHashProvider";
 import { BCryptHashProvider } from "./HashProvider/implementations/BCryptHashProvider";
+import { ITokenProvider } from "./TokenProvider/models/ITokenProvider";
+import { JwtTokenProvider } from "./TokenProvider/implementations/JwtTokenProvider";
 
 container.registerSingleton<IHashProvider>("HashProvider", BCryptHashProvider);
+container.registerSingleton<ITokenProvider>("TokenProvider", JwtTokenProvider);
