6957e5f347b64c29ff00950e2db23f61a4b2ed13
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-10 21:30:39 +0200

    update posts routes

--- src/modules/posts/infra/http/routes/posts.routes.ts
+++ src/modules/posts/infra/http/routes/posts.routes.ts
@@ -1,10 +1,10 @@
-import ensureAuthenticated from "@modules/users/infra/http/middlewares/EnsureAuthenticated";
 import { Router } from "express";
 import { PostsController } from "../controllers/PostsController";
 
 const postsRouter = Router();
 const postsController = new PostsController();
 
-postsRouter.post("/", ensureAuthenticated, postsController.create);
+postsRouter.post("/", postsController.create);
+postsRouter.put("/:post_id", postsController.update);
 
 export { postsRouter };
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import sessionsRouter from "@modules/users/infra/http/routes/sessions.routes";
 import { userRouter } from "@modules/users/infra/http/routes/users.routes";
 import { postsRouter } from "@modules/posts/infra/http/routes/posts.routes";
+import ensureAuthenticated from "@modules/users/infra/http/middlewares/EnsureAuthenticated";
 
 const router = Router();
 
 router.use("/users", userRouter);
 router.use("/sessions", sessionsRouter);
-router.use("/posts", postsRouter);
+router.use("/posts", ensureAuthenticated, postsRouter);
 
 export { router };
