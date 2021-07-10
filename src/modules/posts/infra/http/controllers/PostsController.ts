f0ef5c74e4ce992a1fe8d8c16e041479862db58f
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-10 21:30:27 +0200

    update PostsController: add update

--- src/modules/posts/infra/http/controllers/PostsController.ts
+++ src/modules/posts/infra/http/controllers/PostsController.ts
@@ -1,5 +1,7 @@
 import { CreatePostService } from "@modules/posts/services/CreatePostService";
+import { UpdatePostService } from "@modules/posts/services/UpdatePostService";
 import { CreatePostValidator } from "@modules/posts/validators/CreatePostValidator";
+import { UpdatePostValidator } from "@modules/posts/validators/UpdatePostValidator";
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 
@@ -13,6 +15,21 @@ class PostsController {
         const post = await createPost.execute(request.body);
 
         return response.json(post);
+    }
+
+    async update(request: Request, response: Response): Promise<Response> {
+        const validator = new UpdatePostValidator();
+
+        await validator.validate({ ...request.body, ...request.params });
+
+        const updatePost = container.resolve(UpdatePostService);
+        const updatedPost = await updatePost.execute({
+            author_id: request.body.user_id,
+            post_id: request.params.post_id,
+            content: request.body.content,
+        });
+
+        return response.json({ post: { ...updatedPost } });
     }
 }
 
