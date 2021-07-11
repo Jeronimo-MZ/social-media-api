a35299076d00345b230535700f4e84fb881d1c12
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-11 22:25:41 +0200

    add UpdateUserPasswordService test

---
+++ src/modules/users/services/UpdateUserPasswordService.spec.ts
@@ -0,0 +1,74 @@
+import { AppError } from "@shared/errors/AppError";
+import { FakeHashProvider } from "../providers/HashProvider/fakes/FakeHashProvider";
+import { FakeUsersRepository } from "../repositories/fakes/FakeUsersRepository";
+import CreateUserService from "./CreateUserService";
+import { UpdateUserPasswordService } from "./UpdateUserPasswordService";
+
+describe("UpdateUserPassword", () => {
+    it("should be able to update a user's password", async () => {
+        const hashProvider = new FakeHashProvider();
+        const usersRepository = new FakeUsersRepository();
+        const createUser = new CreateUserService(usersRepository, hashProvider);
+        const updatePassword = new UpdateUserPasswordService(
+            usersRepository,
+            hashProvider,
+        );
+
+        const user = await createUser.execute({
+            email: "user@mail.com",
+            nickname: "username",
+            password: "12345678",
+        });
+
+        await updatePassword.execute(user._id, {
+            oldPassword: user.password,
+            newPassword: "updatedPassword",
+        });
+
+        const updatedUser = await usersRepository.findById(user._id);
+
+        expect(updatedUser).not.toBeUndefined();
+        expect(updatedUser?.password).toBe("updatedPassword");
+    });
+
+    it("should not be able to update the password of a non-existent user", async () => {
+        const hashProvider = new FakeHashProvider();
+        const usersRepository = new FakeUsersRepository();
+        const updatePassword = new UpdateUserPasswordService(
+            usersRepository,
+            hashProvider,
+        );
+
+        await expect(
+            updatePassword.execute("fakeId1234", {
+                oldPassword: "oldPassword",
+                newPassword: "updatedPassword",
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it("should be able to update the password if an wrong old password is provided", async () => {
+        const hashProvider = new FakeHashProvider();
+        const usersRepository = new FakeUsersRepository();
+
+        const createUser = new CreateUserService(usersRepository, hashProvider);
+
+        const updatePassword = new UpdateUserPasswordService(
+            usersRepository,
+            hashProvider,
+        );
+
+        const user = await createUser.execute({
+            email: "user@mail.com",
+            nickname: "username",
+            password: "12345678",
+        });
+
+        await expect(
+            updatePassword.execute(user._id, {
+                oldPassword: "wrongPassword",
+                newPassword: "updatedPassword",
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
