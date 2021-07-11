5c371ad5604cb0bc89a2ef8bcfc080c0a0080758
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-11 22:24:01 +0200

    add CreateUserService test

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,74 @@
+import { AppError } from "@shared/errors/AppError";
+import { FakeHashProvider } from "../providers/HashProvider/fakes/FakeHashProvider";
+import { FakeUsersRepository } from "../repositories/fakes/FakeUsersRepository";
+import CreateUserService from "./CreateUserService";
+
+describe("CreateUser", () => {
+    it("should be able to create a new user", async () => {
+        const hashProvider = new FakeHashProvider();
+        const usersRepository = new FakeUsersRepository();
+        const createUser = new CreateUserService(usersRepository, hashProvider);
+
+        const user = await createUser.execute({
+            email: "user@mail.com",
+            nickname: "username",
+            password: "12345678",
+        });
+
+        expect(user).toHaveProperty("_id");
+    });
+
+    it("should not be able to create user with spaces in the nickname", async () => {
+        const hashProvider = new FakeHashProvider();
+        const usersRepository = new FakeUsersRepository();
+        const createUser = new CreateUserService(usersRepository, hashProvider);
+
+        await expect(
+            createUser.execute({
+                email: "user@mail.com",
+                nickname: "user name",
+                password: "12345678",
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it("should not be able to create user with same email from another", async () => {
+        const hashProvider = new FakeHashProvider();
+        const usersRepository = new FakeUsersRepository();
+        const createUser = new CreateUserService(usersRepository, hashProvider);
+
+        await createUser.execute({
+            email: "user@mail.com",
+            nickname: "username",
+            password: "12345678",
+        });
+
+        await expect(
+            createUser.execute({
+                email: "user@mail.com",
+                nickname: "username2",
+                password: "12345678",
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it("should not be able to create user with same nickname from another", async () => {
+        const hashProvider = new FakeHashProvider();
+        const usersRepository = new FakeUsersRepository();
+        const createUser = new CreateUserService(usersRepository, hashProvider);
+
+        await createUser.execute({
+            email: "user@mail.com",
+            nickname: "username",
+            password: "12345678",
+        });
+
+        await expect(
+            createUser.execute({
+                email: "user2@mail.com",
+                nickname: "username",
+                password: "12345678",
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
