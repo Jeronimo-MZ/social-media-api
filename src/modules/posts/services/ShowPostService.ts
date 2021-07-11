e5b923caae341cd8bfbb90e4bb673652c9b79631
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-11 15:38:31 +0200

    add show post service

---
+++ src/modules/posts/services/ShowPostService.ts
@@ -0,0 +1,23 @@
+import { AppError } from "@shared/errors/AppError";
+import { injectable, inject } from "tsyringe";
+import { IPost } from "../infra/mongoose/models/Post";
+import { IPostsRepository } from "../repositories/IPostsRepository";
+
+@injectable()
+class ShowPostService {
+    constructor(
+        @inject("PostsRepository")
+        private postsRepository: IPostsRepository,
+    ) {}
+
+    async execute(post_id: string): Promise<IPost> {
+        const post = await this.postsRepository.findById(post_id);
+        if (!post) {
+            throw new AppError("Post not found", 404);
+        }
+
+        return post;
+    }
+}
+
+export { ShowPostService };
