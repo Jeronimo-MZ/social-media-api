9efcc0f148c452bb77904fb5e4533a12258d643e
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-11 15:13:59 +0200

    update eslint rules and fix related errors

--- src/modules/posts/services/ToggleLikePostService.ts
+++ src/modules/posts/services/ToggleLikePostService.ts
@@ -2,7 +2,6 @@ import { injectable, inject } from "tsyringe";
 import { IPostsRepository } from "@modules/posts/repositories/IPostsRepository";
 import { IToggleLikePostDTO } from "../dtos/IToggleLikePostDTO";
 import { AppError } from "@shared/errors/AppError";
-import { IPost } from "../infra/mongoose/models/Post";
 
 @injectable()
 class ToggleLikePostService {
