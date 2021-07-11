c5eb0335d11d97b4b5323806e1d022e361c0c36e
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-11 15:11:08 +0200

    move authentication middleware

--- src/modules/posts/infra/http/routes/posts.routes.ts
+++ src/modules/posts/infra/http/routes/posts.routes.ts
@@ -1,14 +1,19 @@
 import { Router } from "express";
 import { PostLikesController } from "../controllers/PostLikesController";
 import { PostsController } from "../controllers/PostsController";
+import ensureAuthenticated from "@modules/users/infra/http/middlewares/EnsureAuthenticated";
 
 const postsRouter = Router();
 const postsController = new PostsController();
 const postLikesController = new PostLikesController();
 
-postsRouter.post("/", postsController.create);
-postsRouter.put("/:post_id", postsController.update);
-postsRouter.patch("/:post_id/like", postLikesController.toggleLike);
-postsRouter.delete("/:post_id", postsController.delete);
+postsRouter.post("/", ensureAuthenticated, postsController.create);
+postsRouter.put("/:post_id", ensureAuthenticated, postsController.update);
+postsRouter.patch(
+    "/:post_id/like",
+    ensureAuthenticated,
+    postLikesController.toggleLike,
+);
+postsRouter.delete("/:post_id", ensureAuthenticated, postsController.delete);
 
 export { postsRouter };
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,12 +2,11 @@ import { Router } from "express";
 import sessionsRouter from "@modules/users/infra/http/routes/sessions.routes";
 import { userRouter } from "@modules/users/infra/http/routes/users.routes";
 import { postsRouter } from "@modules/posts/infra/http/routes/posts.routes";
-import ensureAuthenticated from "@modules/users/infra/http/middlewares/EnsureAuthenticated";
 
 const router = Router();
 
 router.use("/users", userRouter);
 router.use("/sessions", sessionsRouter);
-router.use("/posts", ensureAuthenticated, postsRouter);
+router.use("/posts", postsRouter);
 
 export { router };
