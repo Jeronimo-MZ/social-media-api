ee3f5a81c51e30099a8299adba0673458b91ef9b
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-11 15:06:31 +0200

    update posts routes

--- src/modules/posts/infra/http/routes/posts.routes.ts
+++ src/modules/posts/infra/http/routes/posts.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { PostLikesController } from "../controllers/PostLikesController";
 import { PostsController } from "../controllers/PostsController";
 
 const postsRouter = Router();
 const postsController = new PostsController();
+const postLikesController = new PostLikesController();
 
 postsRouter.post("/", postsController.create);
 postsRouter.put("/:post_id", postsController.update);
+postsRouter.patch("/:post_id/like", postLikesController.toggleLike);
 postsRouter.delete("/:post_id", postsController.delete);
 
 export { postsRouter };
