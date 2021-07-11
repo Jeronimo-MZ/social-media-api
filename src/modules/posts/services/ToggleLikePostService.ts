41d6bf7e51b7e1f874d71e1ff072117d4db1ef5a
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-12 00:06:27 +0200

    update ToggleLikePostService: validate user

--- src/modules/posts/services/ToggleLikePostService.ts
+++ src/modules/posts/services/ToggleLikePostService.ts
@@ -2,12 +2,16 @@ import { injectable, inject } from "tsyringe";
 import { IPostsRepository } from "@modules/posts/repositories/IPostsRepository";
 import { IToggleLikePostDTO } from "../dtos/IToggleLikePostDTO";
 import { AppError } from "@shared/errors/AppError";
+import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
 
 @injectable()
 class ToggleLikePostService {
     constructor(
         @inject("PostsRepository")
         private postsRepository: IPostsRepository,
+
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository,
     ) {}
 
     async execute({ post_id, user_id }: IToggleLikePostDTO): Promise<string> {
@@ -15,6 +19,12 @@ class ToggleLikePostService {
 
         if (!post) {
             throw new AppError("Post not found", 404);
+        }
+
+        const user = await this.usersRepository.findById(user_id);
+
+        if (!user) {
+            throw new AppError("User not found", 403);
         }
 
         if (post.likes.includes(String(user_id))) {
