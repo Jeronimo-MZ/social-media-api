4fd4b06d4ec8b6bc823fa5ad935376124bf6fc83
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-12 10:55:43 +0200

    update FollowUserService: use HttpStatusCode enum

--- src/modules/users/services/FollowUserService.ts
+++ src/modules/users/services/FollowUserService.ts
@@ -2,6 +2,7 @@ import { IFollowUserDTO } from "@modules/users/dtos/IFollowUserDTO";
 import { AppError } from "@shared/errors/AppError";
 import { injectable, inject } from "tsyringe";
 import { IUsersRepository } from "@modules/users/repositories/IUsersRepository";
+import { HttpStatusCode } from "@shared/utils/HttpStatusCode";
 
 @injectable()
 class FollowUserService {
@@ -14,7 +15,10 @@ class FollowUserService {
         followed_user_id,
     }: IFollowUserDTO): Promise<void> {
         if (user_id === followed_user_id) {
-            throw new AppError("You cannot follow yourself");
+            throw new AppError(
+                "You cannot follow yourself",
+                HttpStatusCode.FORBIDDEN,
+            );
         }
 
         const currentUser = await this.usersRepository.findById(user_id);
@@ -22,12 +26,20 @@ class FollowUserService {
             followed_user_id,
         );
 
-        if (!currentUser) throw new AppError("User Not found", 404);
+        if (!currentUser)
+            throw new AppError("User Not found", HttpStatusCode.UNAUTHORIZED);
 
-        if (!followedUser) throw new AppError("Followed user Not found", 404);
+        if (!followedUser)
+            throw new AppError(
+                "Followed user Not found",
+                HttpStatusCode.NOT_FOUND,
+            );
 
         if (currentUser.followings.includes(followed_user_id)) {
-            throw new AppError("You already follow this user", 403);
+            throw new AppError(
+                "You already follow this user",
+                HttpStatusCode.FORBIDDEN,
+            );
         }
 
         await this.usersRepository.followUser({ followed_user_id, user_id });
