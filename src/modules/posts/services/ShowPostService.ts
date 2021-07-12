770fcfdafdf8515d5e1b5706ed9f942f136905cc
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-12 10:49:57 +0200

    update ShowPostService: use HttpStatusCode enum

--- src/modules/posts/services/ShowPostService.ts
+++ src/modules/posts/services/ShowPostService.ts
@@ -1,4 +1,5 @@
 import { AppError } from "@shared/errors/AppError";
+import { HttpStatusCode } from "@shared/utils/HttpStatusCode";
 import { injectable, inject } from "tsyringe";
 import { IPost } from "../infra/mongoose/models/Post";
 import { IPostsRepository } from "../repositories/IPostsRepository";
@@ -13,7 +14,7 @@ class ShowPostService {
     async execute(post_id: string): Promise<IPost> {
         const post = await this.postsRepository.findById(post_id);
         if (!post) {
-            throw new AppError("Post not found!", 404);
+            throw new AppError("Post not found!", HttpStatusCode.NOT_FOUND);
         }
 
         return post;
