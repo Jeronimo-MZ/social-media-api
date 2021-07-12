e738b8af45bf0a66ffb666c5856f620821cd39b0
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-07-12 10:47:30 +0200

    update PostLikeController: use HttpStatusCode enum

--- src/modules/posts/infra/http/controllers/PostLikesController.ts
+++ src/modules/posts/infra/http/controllers/PostLikesController.ts
@@ -1,11 +1,12 @@
 import { ToggleLikePostService } from "@modules/posts/services/ToggleLikePostService";
+import { HttpStatusCode } from "@shared/utils/HttpStatusCode";
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 
 class PostLikesController {
     async toggleLike(request: Request, response: Response): Promise<Response> {
         const toggleLikePost = container.resolve(ToggleLikePostService);
-        return response.status(200).send(
+        return response.status(HttpStatusCode.OK).send(
             await toggleLikePost.execute({
                 post_id: request.params.post_id,
                 user_id: request.body.user_id,
